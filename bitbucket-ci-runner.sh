5aaaf923d8d2dd7aeb011ee2cd52f57aa52982f7
Effie Rozental <effie@snowmate.io>
2023-10-20 18:24:46 -0400

    Initial commit

--- bitbucket-ci-runner.sh
+++ bitbucket-ci-runner.sh
@@ -66,8 +66,8 @@ if [ -n "$SNOWMATE_ACCESS_TOKEN" ]; then
         apt update
         apt-get install jq --assume-yes
         markdown_content=$(cat $SNOWMATE_REPORT_FILE_PATH | jq -s -R .)
-        echo '$PULL_REQUESTS_COMMENTS_API_URL'
-        curl --request POST --url '$PULL_REQUESTS_COMMENTS_API_URL' --header 'Content-Type: application/json' --header 'Authorization: Bearer $SNOWMATE_GITLAB_GROUP_TOKEN' --data "{ \"content\": { \"markup\": $markdown_content } }"
+        echo $PULL_REQUESTS_COMMENTS_API_URL
+        curl --request POST --url $PULL_REQUESTS_COMMENTS_API_URL --header 'Content-Type: application/json' --header 'Authorization: Bearer $SNOWMATE_GITLAB_GROUP_TOKEN' --data "{ \"content\": { \"markup\": $markdown_content } }"
     else
         echo "Snowmate result file was not created, can not create a comment on the pull request"
     fi
